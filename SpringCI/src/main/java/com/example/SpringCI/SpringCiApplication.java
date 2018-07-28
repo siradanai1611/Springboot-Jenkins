80fb6537802c9cfecda8bca38cd0fe84f06be999
iceover <siradanaiboonnak@hotmail.com>
2018-07-28 23:17:20 +0700

    no message

---
+++ SpringCI/src/main/java/com/example/SpringCI/SpringCiApplication.java
@@ -0,0 +1,12 @@
+package com.example.SpringCI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCiApplication.class, args);
+	}
+}
---
+++ SpringCI/src/test/java/com/example/SpringCI/SpringCiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.SpringCI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
